ac952b11c5bb22d2ee8345aa5eccaf9b6e63c35a
Howard <how2945ard@gmail.com>
2019-06-17 21:03:07 +0800

    Throw origninal error

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ exports.getUserByUsername = username => bluebirdRetry(
       })
   ), {
     max_tries: 3,
+    throw_original: true,
     interval: 1000,
     backoff: 2,
   },
@@ -51,6 +52,7 @@ exports.getUserIdFromUsername = username => bluebirdRetry(
       })
   ), {
     max_tries: 3,
+    throw_original: true,
     interval: 1000,
     backoff: 2,
   },
@@ -59,94 +61,155 @@ exports.getUserIdFromUsername = username => bluebirdRetry(
 exports.getMediaByCode = shortcode =>
   bluebirdRetry(() => (
     request.getAsync({ url: `https://www.instagram.com/p/${shortcode}/?__a=1`, json: true })
-      .then(({ body }) => body.graphql)
+      .then(({ body }) => {
+        if (body === 'Oops, an error occurred.\n') {
+          throw new APIError('Oops, an error occurred.');
+        }
+        return body.graphql;
+      })
       .catch(error => error)
   ), {
-    max_tries: 3,
-    interval: 1000,
-    backoff: 2,
-  });
+      max_tries: 3,
+      throw_original: true,
+      interval: 1000,
+      backoff: 2,
+    });
 
 exports.getMediaCommentsByCode = shortcode =>
   bluebirdRetry(() => (
     request.getAsync({ url: `https://www.instagram.com/p/${shortcode}/?__a=1`, json: true })
-      .then(({ body }) => body.graphql.shortcode_media.edge_media_to_comment)
+      .then(({ body }) => {
+        if (body === 'Oops, an error occurred.\n') {
+          throw new APIError('Oops, an error occurred.');
+        }
+        return body.graphql.shortcode_media.edge_media_to_comment;
+      })
       .catch(error => error)
   ), {
-    max_tries: 3,
-    interval: 1000,
-    backoff: 2,
-  });
+      max_tries: 3,
+      throw_original: true,
+      interval: 1000,
+      backoff: 2,
+    });
 
 exports.getUsenameFromUserID = userID =>
   bluebirdRetry(() => (
     request.getAsync({ url: `https://i.instagram.com/api/v1/users/${userID}/info/`, json: true })
-      .then(({ body }) => body.user)
+      .then(({ body }) => {
+        if (body === 'Oops, an error occurred.\n') {
+          throw new APIError('Oops, an error occurred.');
+        }
+        return body.user;
+      })
   ), {
-    max_tries: 3,
-    interval: 1000,
-    backoff: 2,
-  });
+      max_tries: 3,
+      throw_original: true,
+      interval: 1000,
+      backoff: 2,
+    });
 
 exports.getTaggedUsersByCode = shortcode =>
   bluebirdRetry(() => (
     request.getAsync({ url: `https://www.instagram.com/p/${shortcode}/?__a=1`, json: true })
-      .then(({ body }) => body.graphql.shortcode_media.edge_media_to_tagged_user)
+      .then(({ body }) => {
+        if (body === 'Oops, an error occurred.\n') {
+          throw new APIError('Oops, an error occurred.');
+        }
+        return body.graphql.shortcode_media.edge_media_to_tagged_user;
+      })
       .catch(error => error)
   ), {
+      max_tries: 3,
+      throw_original: true,
+      interval: 1000,
+      backoff: 2,
+    });
+
+
+exports.getMediaLikesByCode = shortcode => bluebirdRetry(() => (
+  request.getAsync({ url: `https://www.instagram.com/p/${shortcode}/?__a=1`, json: true })
+    .then(({ body }) => {
+      if (body === 'Oops, an error occurred.\n') {
+        throw new APIError('Oops, an error occurred.');
+      }
+      return body.graphql.shortcode_media.edge_media_preview_like;
+    })
+    .catch(error => error)
+), {
     max_tries: 3,
+    throw_original: true,
     interval: 1000,
     backoff: 2,
   });
 
 
-exports.getMediaLikesByCode = shortcode => bluebirdRetry(() => (
-  request.getAsync({ url: `https://www.instagram.com/p/${shortcode}/?__a=1`, json: true })
-    .then(({ body }) => body.graphql.shortcode_media.edge_media_preview_like)
-    .catch(error => error)
-), {
-  max_tries: 3,
-  interval: 1000,
-  backoff: 2,
-});
-
-
 exports.getMediaOwnerByCode = shortcode => bluebirdRetry(() => (
   request.getAsync({ url: `https://www.instagram.com/p/${shortcode}/?__a=1`, json: true })
-    .then(({ body }) => body.graphql.shortcode_media.owner)
+    .then(({ body }) => {
+      if (body === 'Oops, an error occurred.\n') {
+        throw new APIError('Oops, an error occurred.');
+      }
+      return body.graphql.shortcode_media.owner;
+    })
     .catch(error => error)
 ), {
-  max_tries: 3,
-  interval: 1000,
-  backoff: 2,
-});
+    max_tries: 3,
+    throw_original: true,
+    interval: 1000,
+    backoff: 2,
+  });
 
 
 exports.getMediaByLocation = (locationId, maxId = '') =>
   bluebirdRetry(() => (
     request.getAsync({ url: `https://www.instagram.com/explore/locations/${locationId}/?__a=1&max_id=${maxId}`, json: true })
-      .then(({ body }) => body.graphql)
+      .then(({ body }) => {
+        if (body === 'Oops, an error occurred.\n') {
+          throw new APIError('Oops, an error occurred.');
+        }
+        return body.graphql;
+      })
       .catch(error => error)
   ), {
-    max_tries: 3,
-    interval: 1000,
-    backoff: 2,
-  });
+      max_tries: 3,
+      throw_original: true,
+      interval: 1000,
+      backoff: 2,
+    });
 
 
 exports.getHashInfoByTag = (tag, maxId = '') =>
   bluebirdRetry(() => (
     request.getAsync({ url: `https://www.instagram.com/explore/tags/${urlencode(tag)}/?__a=1&max_id=${maxId}`, json: true })
-      .then(({ body }) => body.graphql)
+      .then(({ body }) => {
+        if (body === 'Oops, an error occurred.\n') {
+          throw new APIError('Oops, an error occurred.');
+        }
+        return body.graphql;
+      })
       .catch(error => error)
   ), {
-    max_tries: 3,
-    interval: 1000,
-    backoff: 2,
-  });
+      max_tries: 3,
+      throw_original: true,
+      interval: 1000,
+      backoff: 2,
+    });
 
 
-exports.generalSearch = query => (
-  request.getAsync({ url: `https://www.instagram.com/web/search/topsearch/?query=${urlencode(query)}`, json: true })
-    .then(({ body }) => body))
-  .catch(error => error);
+exports.generalSearch = query =>
+  bluebirdRetry(() => (
+    request.getAsync({ url: `https://www.instagram.com/web/search/topsearch/?query=${urlencode(query)}`, json: true })
+      .then(({ body }) => {
+        if (body === 'Oops, an error occurred.\n') {
+          throw new APIError('Oops, an error occurred.');
+        }
+        return body;
+      })
+      .catch(error => error), {
+      max_tries: 3,
+      throw_original: true,
+      interval: 1000,
+      backoff: 2,
+    })
+  )
+
